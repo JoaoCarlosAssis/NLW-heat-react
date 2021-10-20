8adfc2bcbe9c7070ed98619bd559ce3d86274152
Joao Carlos Assis <carlos.rocha.assis@gmail.com>
2021-10-20 16:26:40 -0300

    feat: get data in backend (messages)

--- src/components/loginBox/index.tsx
+++ src/components/loginBox/index.tsx
@@ -1,10 +1,11 @@
+import { VscGithubInverted } from 'react-icons/vsc'
 import styles from './styles.module.scss'
-export function LoginBox(){
-  return(
+export function LoginBox() {
+  return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe a sua mensagem</strong>
       <a href="#" className={styles.signInWithGithub}>
-        Entrar com Github
+        <VscGithubInverted /> Entrar com Github
       </a>
     </div>
   )
--- src/components/messageList/index.tsx
+++ src/components/messageList/index.tsx
@@ -1,5 +1,48 @@
+import {api} from '../../services/api'
+import logoImg from '../../assets/logo.svg'
+import styles from './styles.module.scss';
+import { useEffect, useState } from 'react';
+
+type Message = {
+  id: string;
+  text: string;
+  user:{
+    name: string;
+    avatar_url: string;
+  }
+}
+
 export function MessageList(){
+
+  const [messages, setMessages] = useState<Message[]>([])
+
+  useEffect(()=>{
+    api.get<Message[]>("messages/last3").then(response => {
+      setMessages(response.data)
+    })
+  }, [])
+
   return(
-    <h1>messageList</h1>
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+      
+      <ul className={styles.messageList}>
+        {messages.map(message => (
+          <li className={styles.message} key={message.id}>
+          <p className={styles.messageContent}>{message.text}</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImg}>
+              <img src={message.user.avatar_url} alt={message.user.name} />
+            </div>
+            <span>{message.user.name}</span>
+          </div>
+        </li>
+        ))}
+        
+
+    
+
+      </ul>
+    </div>
   )
 }
